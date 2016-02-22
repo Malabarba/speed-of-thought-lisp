68160bad14b031b069f30f1bd494ba4f252321c1
Artur Malabarba <bruce.connor.am@gmail.com>
2016-02-22 13:13:45 -0300

    Version bump

--- sotlisp.el
+++ sotlisp.el
@@ -6,7 +6,7 @@
 ;; URL: https://github.com/Malabarba/speed-of-thought-lisp
 ;; Keywords: convenience, lisp
 ;; Package-Requires: ((emacs "24.1"))
-;; Version: 1.5
+;; Version: 1.5.1
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
